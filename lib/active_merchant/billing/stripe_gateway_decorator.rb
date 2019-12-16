8640cb6c1bdd9253764ab2c3bf413e38f427f4fc
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-12-16 14:06:34 +0100

    Adds x-stripe-client-user-agent data to stripe_gateway_decorator.rb

--- lib/active_merchant/billing/stripe_gateway_decorator.rb
+++ lib/active_merchant/billing/stripe_gateway_decorator.rb
@@ -5,6 +5,7 @@ module ActiveMerchant
 
       def headers(options = {})
         headers = super
+        headers['X-Stripe-Client-User-Agent'] = x_stripe_client_user_agent_data
         headers['User-Agent'] = headers['X-Stripe-Client-User-Agent']
         headers
       end
@@ -12,6 +13,19 @@ module ActiveMerchant
       def add_customer_data(post, options)
         super
         post[:payment_user_agent] = "SpreeGateway/#{SpreeGateway.version}"
+      end
+
+      def x_stripe_client_user_agent_data
+        "{
+          'lang': 'ruby',
+          'publisher': 'SpreeGateway',
+          'application': {
+            'name': 'SpreeGateway'
+            'version': #{SpreeGateway.version},
+            'partner_id': 'pp_partner_FC3KpLMMQgUgcQ',
+            'url': 'spreecommerce.org',
+          }
+        }"
       end
     end
   end
